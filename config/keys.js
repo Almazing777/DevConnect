793cb26392c342d9313282e1ebda4ddd460806e2
almas <mendygax@gmail.com>
2019-01-29 21:45:09 -0500

    implemented Json web tokens

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://almasmendy:twdAM2019!@ds213705.mlab.com:13705/databaseconnector777"
+    "mongodb://almasmendy:twdAM2019!@ds213705.mlab.com:13705/databaseconnector777",
+  secretOrKey: "secret"
 };
