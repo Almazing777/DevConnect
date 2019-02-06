2cac0dbdf43f1e3b866efbc4e0031dc43eb5f85e
almas <mendygax@gmail.com>
2019-02-05 19:27:54 -0500

    Profile creating logic

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -68,6 +68,22 @@ router.post(
     if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
     if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
     if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        //Update
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        //Create
+
+        //Check if handle exists
+        Profile.findOne({ handle: profileFields.handle });
+      }
+    });
   }
 );
 module.exports = router;
